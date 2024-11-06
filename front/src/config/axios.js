6f9e285c22a4cf49ed2f913ee8aba744cdeeb543
torokokoo <torokokov7@gmail.com>
2024-11-06 09:24:42 -0300

    modified backend ip and added CORS config

--- front/src/config/axios.js
+++ front/src/config/axios.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const BACKEND_URL = import.meta.env.VITE_BACKEND_URL 
+const BACKEND_URL = import.meta.env.VITE_BACKEND_URL || '52.233.92.96'
 const BACKEND_PORT = import.meta.env.VITE_BACKEND_PORT 
 
 console.log(`Conectado al backend en ${BACKEND_URL}:${BACKEND_PORT}`)
